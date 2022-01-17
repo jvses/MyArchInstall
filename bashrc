a217f84e0d907f8677f6f89b0af5478c460c58fb
jvses <180137221@aluno.unb.br>
2022-01-17 09:46:00 -0300

    Alterações nos arquivos e no bashrc

--- bashrc
+++ bashrc
@@ -10,8 +10,23 @@ alias la='ls -a'
 alias ll='la -l'
 alias yt-mpv360='mpv --ytdl-format=134+bestaudio'
 alias yt-mpv480='mpv --ytdl-format=135+bestaudio'
+#alias mmpv='mpv'
+
+
+# alias para o devour
+alias dvince='devour evince'
+alias dmpv='devour mpv'
+alias imv-folder='devour imv-folder'
+alias imv='devour imv'
+
+
 
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 
 pokemon-colorscripts -r
+
+
+# BEGIN_KITTY_SHELL_INTEGRATION
+if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; then source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; fi
+# END_KITTY_SHELL_INTEGRATION
--- install.sh
+++ install.sh
@@ -4,19 +4,17 @@
 #sudo hwclock --systohc
 
 # Sincronizando os servidores de pacotes mais rápidos
-sudo reflector -c Brazil -a 48 --sort rate --save /etc/pacman.d/mirrorlist
+#sudo reflector -c Brazil -a 48 --sort rate --save /etc/pacman.d/mirrorlist
 
 #Instalando pacotes extras que podem ter faltado, Isso foi copidado do Ermanno Ferrari, ao menos a maioria
 
-sudo pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font lolcat cmatrix cowsay neofeth figlet xf86-video-intel xorg i3-gaps firefox rofi terminator dmenu terminator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings htop bashtop feh lxappearance thunar nautilus pavucontrol file-roller mpv imv git vim midori discord arandr xorg-xrandr arduino evince gedit flatpak tlp clamav clamtk
+sudo pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font lolcat cmatrix cowsay neofeth figlet xf86-video-intel xorg i3-gaps firefox rofi terminator dmenu terminator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings htop bashtop feh lxappearance thunar nautilus pavucontrol file-roller mpv imv git vim midori discord arandr xorg-xrandr arduino evince gedit flatpak tlp clamav clamtk
 
 #Instalando fontes
-sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fira-mono ttf-hack ttf-fira-code ttf-jetbrains-mono adobe-source-code-pro-fonts cantarell-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
-
-
+#sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fira-mono ttf-hack ttf-fira-code ttf-jetbrains-mono adobe-source-code-pro-fonts cantarell-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 #Instalado drivers gráficos,Display manager, DE e outras coisas
-#sudo pacman -S --noconfirm lolcat cmatrix cowsay neofeth figlet xf86-video-intel xorg i3-gaps firefox rofi terminator dmenu terminator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings htop bashtop feh lxappearance thunar nautilus pavucontrol file-roller mpv imv git vim midori discord arandr xorg-xrandr arduino evince gedit flatpak tlp clamav clamtk
+# sudo pacman -S lolcat cmatrix cowsay neofeth figlet xf86-video-intel xorg i3-gaps firefox rofi terminator dmenu terminator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings htop bashtop feh lxappearance thunar nautilus pavucontrol file-roller mpv imv git vim midori discord arandr xorg-xrandr arduino evince gedit flatpak tlp clamav clamtk
 
 # Atualizando as definições de vírus do anti-vírus
 
@@ -35,8 +33,10 @@ sudo systemctl enable clamav-daemon.service
 #cd yay
 #makepkg -si PKGBUILD
 
+# ; slack-desktop telegram-desktop-bin video-downloader pdfarranger-git handbrake-full picom-jonaburg-git i3lock-color teams bottles projectlibre drawio-desktop odysee-nativefier 
+
 # pacotes para instalar da AUR
-# xinit-xsession teams ytfzf bottles drawio handbrake odisee i3lock-color polyba pdf-arranger picomjo o instalador de fontes projectlibre brave vscode telegram slack inkscape videodownloader spotify gotp 
+# xinit-xsession teams ytfzf bottles drawio handbrake odisee i3lock-color polybar pdf-arranger picomjo o instalador de fontes projectlibre brave vscode telegram slack inkscape videodownloader spotify gotp 
 /bin/echo -e "\e[1;32mREINICIANDO EM 5..4..3..2..1..\e[0m"
 sleep 5
 sudo reboot
